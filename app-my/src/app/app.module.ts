f76a575f3d55d48463bd0aca5015a5b52da55584
jagdish58 <jjj.kumar58@gmai.com>
2023-04-22 00:05:03 +0530

    send data

--- app-my/src/app/app.module.ts
+++ app-my/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServiceNameService } from './service/common.service';
+import {HttpClientModule  } from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ReactiveFormsModule,HttpClientModule, FormsModule
   ],
-  providers: [],
+  providers: [ServiceNameService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ app-my/src/app/service/common.service.ts
@@ -0,0 +1,85 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class ServiceNameService {
+uers:any="https://dummyjson.com/products/search?q";
+    mocksD:any = [
+        {
+            questionsName: 'what is your age?',
+            questiontypeId: 1,
+            options: [
+                {   age:13,
+                    id: 1
+                },
+                {   age:29,
+                    id: 2
+                },
+                {   age:30,
+                    id: 3
+                },
+                {   age:20,
+                    id: 4
+                }
+            ]
+
+        },
+        {
+            questionsName: 'what is your technology?',
+            questiontypeId: 2,
+            options: [
+                {   tech:'javascript',
+                },
+                {   tech:'java',
+
+                },
+                {   tech:'.net',                    
+                },
+                {   tech:'pythons'
+                }
+            ]
+
+        },
+        {
+            questionsName: 'select gender?',
+            questiontypeId: 3,
+           
+
+        },
+        {
+            questionsName: 'select multiple city is your technology?',
+            questiontypeId: 4,
+            options: [
+                {   tech:'pune',
+                },
+                {   tech:'mumbai',
+
+                },
+                {   tech:'us',                    
+                },
+                {   tech:'ca'
+                },
+                {   tech:'bal',
+                },
+                {   tech:'del',
+
+                },
+                {   tech:'chennai',                    
+                },
+                {   tech:'hyd'
+                }
+            ]
+
+        },
+    ]
+    constructor(private http: HttpClient) { }
+
+    getServices(){
+        return this.http.get(this.mocksD)
+    }  
+    getServicesList(data:any):Observable<any>{
+        return this.http.get(this.uers + '=' +data)
+    }
+    
+}
